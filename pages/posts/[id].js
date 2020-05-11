076d707526013686ba662482e884c127ab6d6923
baojie <baojie223@qq.com>
2020-05-11 17:39:30 +0800

    :tada: start!

---
+++ pages/posts/[id].js
@@ -0,0 +1,40 @@
+import Layout from '../../components/layout'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+import utilStyles from '../../styles/utils.module.css'
+
+export function getStaticPaths() {
+  const paths = getAllPostIds()
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export default function Post({ postData: { title, contentHtml, date } }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
---
+++ pages/posts/first-post.js
@@ -0,0 +1,5 @@
+function FirstPost() {
+  return <h1>first-post</h1>
+}
+
+export default FirstPost
